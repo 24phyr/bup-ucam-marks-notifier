6fe81ffdc3ccd67e337c65f971241b3dff24a28e
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2025-12-29 03:58:40 +0600

    Initial commit again

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bup-ucam-automation-playground
-Another BUP related repo. sigh
+
+This project is intended to mess around with automation using Selenium. I might have a good plan with this but not at this moment.
---
+++ ucam.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+browser = webdriver.Firefox()
+browser.get('https://ucam.bup.edu.bd/')
+
+user_elem = browser.find_element(By.ID, 'UserId')
+user_elem.send_keys('USERID')
+
+password_elem = browser.find_element(By.ID, 'Password')
+password_elem.send_keys('PASSWORD')
+password_elem.submit()
+
+WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'ctl00_lblLogout')))
+print(browser.find_element(By.ID, 'ctl00_lblAvatarName').text)
+
+examMark_elem = browser.find_element(By.LINK_TEXT, 'Current Exam Mark')
+examMark_elem.click()
+
+# WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/form/div[3]/div[3]/div/div/div/div[1]/label')))
+
+select_element = browser.find_element(By.NAME, 'ctl00$MainContainer$ddlSession')
+select = Select(select_element)
+select.select_by_value('1066')
