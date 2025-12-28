e22bcfa85e1a932da741af6c6ea3b8d7d1c686c6
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2025-12-29 05:08:48 +0600

    Implement dotenv

--- ucam.py
+++ ucam.py
@@ -4,14 +4,18 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 browser = webdriver.Firefox()
 browser.get('https://ucam.bup.edu.bd/')
 
 user_elem = browser.find_element(By.ID, 'UserId')
-user_elem.send_keys('USERID')
+user_elem.send_keys(os.environ["USERID"])
 
 password_elem = browser.find_element(By.ID, 'Password')
-password_elem.send_keys('PASSWORD')
+password_elem.send_keys(os.environ["PASSWORD"])
 password_elem.submit()
 
 WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'ctl00_lblLogout')))
